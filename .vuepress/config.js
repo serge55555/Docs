9c4d9f39f41e3d7a3ca2bf6e616ebb657622abfb
serge55555 <serge55555@hotmail.fr>
2019-12-12 06:46:08 +0100

    Update config.js

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -6,7 +6,7 @@
 }); */
 
 module.exports = {
-  base: "/docs" // For travis to publish
+  base: "/docs", // For travis to publish
   evergreen: true, // Désactive la compatibilité pour les anciens navigateurs
                   // Gain de temps en build
   dest: "docs",
